fdf4f2bc67ccd1cca90a37f0fb77d06dcf5c7d02
freiburgermsu <freiburgermsu@aol.com>
2022-01-26 23:42:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open('README.rst') as file:
     readme = file.read()
 
 setup(
-  name = 'ChemW',      
-  package_dir = {'mw':'chemw'},
+  name = 'wcmpy',      
+  package_dir = {'wcm':'wcmpy'},
   packages = find_packages(),
-  package_data = {
-	'test':['databases/*'],
-  },
-  version = '0.1.2',
+#   package_data = {
+# 	'test':['databases/*'],
+#   },
+  version = '0.0.1',
   license = 'MIT',
-  description = "Calculates the Molecular Weight, to the appropriate significant digits, from a string of an arbitrary chemical formula.", 
+  description = "Simulates a range of biochemical processes of a cocci bacterium over time.", 
   long_description = readme,
   author = 'Andrew Freiburger',               
   author_email = 'andrewfreiburger@gmail.com',
   url = 'https://github.com/freiburgermsu/chemw',   
-  keywords = ['chemistry', 'math', 'mass', 'weight', 'PHREEQC', 'molecular', 'mineral', 'formula', 'calculate'],
-  install_requires = ['chemicals', 'pandas']
+  keywords = ['chemistry', 'molecular', 'biology', 'whole-cell', 'computational', 'modeling'],
+  install_requires = ['scipy', 'codons', 'pandas']
 )
